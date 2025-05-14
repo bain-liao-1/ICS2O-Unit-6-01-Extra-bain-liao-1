7e8c627f19e20ee904d96e7fe44f203d61f901b1
bain-liao-1 <liao.bain@stu.ocsb.ca>
2025-05-13 21:05:31 -0400

    created 2nd pwa

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Bain Liao
+// Created on: May 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function conducts multiplication with addition.
+ */
+function multiplyThroughAddition() {
+  const factor = parseInt(document.getElementById("factor").value)
+  let multiplier = parseInt(document.getElementById("multiplier").value)
+  let product = 0
+
+  while (multiplier > 0) {
+    product = product + factor
+    multiplier = multiplier - 1
+  }
+
+  // Display result
+  document.getElementById("result").innerText = "The product is " + product + "."
+}
